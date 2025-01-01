f1f496647252fc8dcf146ba5304ea17b17806c48
Uhdahlah <89252558+Uhdahlah@users.noreply.github.com>
2025-01-01 23:49:04 +0700

    fix bug

--- query_data.py
+++ query_data.py
@@ -8,13 +8,13 @@ from langchain.prompts import ChatPromptTemplate
 CHROMA_PATH = "chroma"
 
 PROMPT_TEMPLATE = """
-Answer the question based only on the following context:
+answer the question based only on the following context:
 
 {context}
 
 ---
 
-Answer the question based on the above context: {question}
+answer the question based on the above context: {question}
 """
 
 
